294060b5cce9bb0d1cb90075f0e65f4f993fbffa
Jackbaklava <mhqadir14@gmail.com>
2022-02-26 20:17:35 +0500

    Can calculate the determinant and adjoint of a matrix

--- python-scripts/matrix.py
+++ python-scripts/matrix.py
@@ -1,11 +1,25 @@
 class Matrix:
-    def __init__(self, structure):
+    def __init__(self, structure, is_adjoint=False):
         self.structure = structure
-        self.order = f"{len(self.structure)} × {len(self.structure[0])}"
-        self.determinant = "smh"
-        self.adjoint = "smh"
+        self.order = [len(self.structure), len(self.structure[0])]
+        # As of right now, the determinant and adjoint can only be calculated of a 2 * 2 matrix
+        self.determinant = (
+            self.structure[0][0] * self.structure[-1][-1]
+            - self.structure[0][-1] * self.structure[-1][0]
+        )
 
-    def display(self):
+        # Could maybe make another class for the adjoint?
+
+        if is_adjoint == False:
+            self.adjoint = Matrix(
+                [
+                    [self.structure[-1][-1], -self.structure[0][-1]],
+                    [-self.structure[-1][0], self.structure[0][0]],
+                ],
+                is_adjoint=True,
+            )
+
+    def display_structure(self):
         print("[")
         for row in self.structure:
             row_string = "  ["
@@ -19,6 +33,9 @@ class Matrix:
             print(row_string)
 
         print("]")
+
+    def display_order(self):
+        print(f"{self.order[0]} × {self.order[1]}")
 
     def is_equal_to(self):
         pass
@@ -37,13 +54,11 @@ class Matrix:
         pass
 
 
-sample = Matrix(
-    [
-        [1, 2],
-        [3, 4],
-    ]
-)
+sample = Matrix([[1, 2], [3, 4]])
 # Same thing as [ [1, 2], [3, 4] ]
 
-sample.display()
-print(sample.order)
+sample.display_structure()
+sample.display_order()
+print(sample.determinant)
+sample.adjoint.display_structure()
+print(sample.adjoint.determinant)
