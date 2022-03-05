88758193df25a134b35b446c8b0007c8bdec2d20
Jackbaklava <mhqadir14@gmail.com>
2022-03-05 12:21:31 +0500

    Turned determinant and adjoint properties into functions, replaced display_structure with __str__, and added order_string

--- python-scripts/matrix.py
+++ python-scripts/matrix.py
@@ -1,41 +1,39 @@
 class Matrix:
-    def __init__(self, structure, is_adjoint=False):
+    def __init__(self, structure):
         self.structure = structure
         self.order = [len(self.structure), len(self.structure[0])]
-        # As of right now, the determinant and adjoint can only be calculated of a 2 * 2 matrix
-        self.determinant = (
+        self.order_string = f"{self.order[0]} × {self.order[1]}"
+
+    def __str__(self):
+        matrix_string = "["
+
+        for row in self.structure:
+            matrix_string += "\n" + "  ["
+
+            for element in row:
+                matrix_string += str(element)
+                if element == row[-1]:
+                    matrix_string += "]"
+                else:
+                    matrix_string += ", "
+
+        matrix_string += "\n" + "]"
+
+        return matrix_string
+
+    def adjoint(self):
+        return Matrix(
+            [
+                [self.structure[-1][-1], -self.structure[0][-1]],
+                [-self.structure[-1][0], self.structure[0][0]],
+            ]
+        )
+
+    def determinant(self):
+        return (
             self.structure[0][0] * self.structure[-1][-1]
             - self.structure[0][-1] * self.structure[-1][0]
         )
-
-        # Could maybe make another class for the adjoint?
-
-        if is_adjoint == False:
-            self.adjoint = Matrix(
-                [
-                    [self.structure[-1][-1], -self.structure[0][-1]],
-                    [-self.structure[-1][0], self.structure[0][0]],
-                ],
-                is_adjoint=True,
-            )
-
-    def display_structure(self):
-        print("[")
-        for row in self.structure:
-            row_string = "  ["
-            for element in row:
-                row_string += str(element)
-                if element == row[-1]:
-                    row_string += "]"
-                else:
-                    row_string += ", "
-
-            print(row_string)
-
-        print("]")
-
-    def display_order(self):
-        print(f"{self.order[0]} × {self.order[1]}")
 
     def is_equal_to(self):
         pass
@@ -57,8 +55,6 @@ class Matrix:
 sample = Matrix([[1, 2], [3, 4]])
 # Same thing as [ [1, 2], [3, 4] ]
 
-sample.display_structure()
-sample.display_order()
-print(sample.determinant)
-sample.adjoint.display_structure()
-print(sample.adjoint.determinant)
+print(sample)
+print(sample.order_string)
+print(sample.determinant())
